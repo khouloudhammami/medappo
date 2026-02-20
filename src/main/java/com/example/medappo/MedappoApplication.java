9a82174b77efc70b7ad1cb4efab4faca91a2f90e
Khammami-dev <hammamikouloud9@gmail.com>
2026-02-20 02:15:12 +0100

    add dependncies pom.xml

---
+++ src/main/java/com/example/medappo/MedappoApplication.java
@@ -0,0 +1,13 @@
+package com.example.medappo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedappoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedappoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/medappo/MedappoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medappo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedappoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
